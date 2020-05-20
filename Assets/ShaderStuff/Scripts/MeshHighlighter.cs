cf03d0e31ed366d34ff5ed8b31a7eee4248479ed
Erik <ehawg3@gmaill.com>
2020-05-20 12:27:46 -0600

    Init

---
+++ Assets/ShaderStuff/Scripts/MeshHighlighter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshHighlighter : MonoBehaviour
+{
+    private MeshRenderer originalMesh;
+    [SerializeField]
+    private MeshRenderer highlightMesh;
+
+    void Start()
+    {
+        originalMesh = GetComponent<MeshRenderer>();
+        EnableHighlight(false);
+    }
+
+    public void EnableHighlight(bool onOff)
+    {
+        if(highlightMesh != null)
+        {
+            highlightMesh.enabled = onOff;
+            originalMesh.enabled = !onOff;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        EnableHighlight(true);
+    }
+
+    private void OnMouseExit()
+    {
+        EnableHighlight(false);
+    }
+}
